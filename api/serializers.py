d92a3982c49c9417c4b4c9731292a00f1194df75
Yeondam Park <giyen9088@gmail.com>
2020-07-07 22:52:37 -0700

    add comment serialzier

--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,20 @@
 from rest_framework import serializers
 from resources.models import Resources
+from comments.models import Comments
 from taggit_serializer.serializers import (TagListSerializerField,
                                            TaggitSerializer)
 
 
+class CommentSerialzier(serializers.ModelSerialzier):
+
+    class Meta:
+        model = Comments
+        fields = '__all__'
+
 class ResoucesSerializer(serializers.ModelSerializer):
 
     tags = TagListSerializerField()
+    comments = CommentSerialzier()
 
     class Meta:
         model = Resources
@@ -20,4 +28,5 @@ class ResoucesSerializer(serializers.ModelSerializer):
             'thumbnail',
             'slug',
             'url',
+            'comments',
         )
